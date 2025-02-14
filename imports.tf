33a27ba76feed61f73aefba5396eab1f539146de
Peter Rees <peterrees@Peters-MacBook-Pro.local>
2025-02-14 23:02:52 +0000

    initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+# ##################################################################################
+# # IMPORTS
+# ##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-023a8bff5eaf034c9" #VPC
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-0c8d367a5341cd3e5" #PublicSubnet1
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-057eaf7b31e2fd782" #PublicSubnet2
+# }
+
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-0430d071d5c31cdeb" #InternetGateway
+# }
+
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-0362412f7fc28fa65_0.0.0.0/0" #DefaultPublicRoute
+# }
+
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-0362412f7fc28fa65" #PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-0c8d367a5341cd3e5/rtb-0362412f7fc28fa65" #PublicSubnet1/PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-057eaf7b31e2fd782/rtb-0362412f7fc28fa65" #PublicSubnet2/PublicRouteTable
+# }
+
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-0dd7e5ff56b8c71e1" #NoIngressSecurityGroup
+# }
